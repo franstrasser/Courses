940a340bebcf7185993791247d773e04e03df59e
Fran Strasser <59162810+franstrasser@users.noreply.github.com>
2021-06-11 15:54:11 +0200

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Fran Štrasser on 03.03.2021..
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q:String,a:String) {
+        text=q
+        answer=a
+    }
+}

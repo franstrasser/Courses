cdade91e3f69532651127564bd20f651fa6aee57
Fran Strasser <59162810+franstrasser@users.noreply.github.com>
2021-06-11 15:40:56 +0200

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Fran Štrasser on 24.02.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
